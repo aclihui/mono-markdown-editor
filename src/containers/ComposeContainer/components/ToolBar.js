94ead32727024284c2a86120f9f61de9f2bf0611
jixwang <jixwang@thoughtworks.com>
2018-07-18 16:05:16 +0800

    开始做登录功能

---
+++ src/containers/ComposeContainer/components/ToolBar.js
@@ -0,0 +1,15 @@
+import React,{Component} from "react";
+
+class ToolBar extends Component{
+    render(){
+        return (
+            <div className="toolbar">
+                <ul className="user">
+                    <li>登录</li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default ToolBar;
--- src/containers/ComposeContainer/index.js
+++ src/containers/ComposeContainer/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import MarkdownEditor from "./components/MarkdownEditor";
 import Previewer from "./components/Previewer";
+import ToolBar from "./components/ToolBar";
 
 class ComposeContainer extends Component{
     render(){
         return (
             <div className="mono">
+                <ToolBar/>
                 <MarkdownEditor/>
                 <Previewer/>
             </div>
